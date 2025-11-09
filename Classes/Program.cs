10650f5a5dcee8a298f0f836aa00b23cadda147c
michaelsong95@yahoo.com <michaelsong95@yahoo.com>
2025-11-09 15:29:29 -0800

    Completed the exerc ise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car a = new Car();
+            a.Make = "Hyundai";
+            a.Model = "Sonata";
+            a.Year = 2023;
+            Console.WriteLine("This "+a.Make+" "+a.Model+" is made in "+a.Year);
         }
+    }
+
+    public class Car
+    {
+        private int y;
+        public Car()
+        {
+            
+        }
+        
+
+        public String Make
+        {
+            get; set;
+            
+        }
+        public String Model
+        {
+            get; set;
+            
+        }
+        public int Year
+        {
+            get; set;
+            
+        }
+        
     }
 }
