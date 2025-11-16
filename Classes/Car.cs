b16cfcbd53e8e3ffb502143dc2f06ab37fbee793
michaelsong95@yahoo.com <michaelsong95@yahoo.com>
2025-11-15 17:17:52 -0800

    added a separate file for cars

---
+++ Classes/Car.cs
@@ -0,0 +1,25 @@
+﻿public class Car
+{
+    private int y;
+    public Car()
+    {
+            
+    }
+    
+    public String Make
+    {
+        get; set;
+            
+    }
+    public String Model
+    {
+        get; set;
+            
+    }
+    public int Year
+    {
+        get; set;
+            
+    }
+        
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -12,30 +12,5 @@
         }
     }
 
-    public class Car
-    {
-        private int y;
-        public Car()
-        {
-            
-        }
-        
-
-        public String Make
-        {
-            get; set;
-            
-        }
-        public String Model
-        {
-            get; set;
-            
-        }
-        public int Year
-        {
-            get; set;
-            
-        }
-        
-    }
+    
 }
